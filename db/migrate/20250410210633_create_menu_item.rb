29485f9d6f9c8b51d6b091f1a787738e1a363ece
gabrielkx <gabrielgamersbr@gmail.com>
2025-04-10 18:56:40 -0300

    fix: menu foreign key on MenuItem migration

--- db/migrate/20250410210633_create_menu_item.rb
+++ db/migrate/20250410210633_create_menu_item.rb
@@ -3,6 +3,7 @@ class CreateMenuItem < ActiveRecord::Migration[7.2]
     create_table :menu_items do |t|
       t.string :name
       t.decimal :price, precision: 10, scale: 2
+      t.references :menu, null: false, foreign_key: true
 
       t.timestamps
     end

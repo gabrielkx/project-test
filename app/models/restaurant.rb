a9024af4e9b0c03ac81310e65b2fc3a5e315856d
gabrielkx <gabrielgamersbr@gmail.com>
2025-04-11 00:10:14 -0300

    chore: added cascade deletion

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
-  has_many :menus
-  has_many :menu_items, through: :menus
+  has_many :menus, dependent: :destroy
+  has_many :menu_items, through: :menus, dependent: :destroy
 
   accepts_nested_attributes_for :menus
 
